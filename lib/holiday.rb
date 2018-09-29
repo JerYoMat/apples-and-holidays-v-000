aa6144afe8c44183ef1a348a8aaafe75132334d8
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-29 20:39:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,41 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+holiday_hash[:summer][:fourth_of_july][1]
+
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter][:christmas] << supply 
+holiday_hash[:winter][:new_years] << supply 
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash[:spring][:memorial_day] << supply 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = [supply_array] 
+  holiday_hash[season][holiday_name]
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+    supplies = []
+    holiday_hash[:winter].each do |holiday, supply_array|
+      supply_array.each do |supply|
+        supplies << supply 
+      end 
+    end 
+    supplies 
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,6 +63,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday_with_details|
+    puts season
+    puts holiday_with_details
+  end 
 
 end
 
